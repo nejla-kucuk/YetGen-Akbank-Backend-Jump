daa300b9f74536377d32be08dfac7a5fe6c3577c
nkucukk <nkucuk097@gmail.com>
2023-10-05 21:16:59 +0300

    commit week04

---
+++ W4.2-PWGenerator/W4.2-PWGenerator/PasswordGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W4._2_PWGenerator
+{
+    public class PasswordGenerator
+    {
+        private readonly Random _random;
+
+        private const string Numbers = "0123456789";
+        private const string SpecialChars = "!@#$%^&*()";
+        private const string LowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+        private const string UpperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string Full = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()";
+
+        public PasswordGenerator()
+        {
+            _random = new Random();
+        }
+
+        public string Generate(int passwordLength, bool includeNumbers, bool includeLowerCase, bool includeUpperCase, bool includeSpecialChars)
+        {
+            var charsBuilder = new StringBuilder();
+
+            if (includeNumbers)
+                charsBuilder.Append(Numbers);
+
+            if (includeLowerCase)
+                charsBuilder.Append(LowerCaseChars);
+
+            if (includeUpperCase)
+                charsBuilder.Append(UpperCaseChars);
+
+            if (includeSpecialChars)
+                charsBuilder.Append(SpecialChars);
+
+            var acceptedChars = charsBuilder.ToString();
+
+            var passwordBuilder = new StringBuilder();
+
+
+            for (int i = 0; i < passwordLength; i++)
+            {
+                var randomIndex = _random.Next(0, acceptedChars.Length);
+
+                passwordBuilder.Append(acceptedChars[randomIndex]);
+            }
+
+            return passwordBuilder.ToString();
+        }
+
+    }
+}
---
+++ W4.2-PWGenerator/W4.2-PWGenerator/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+var passwordGenerator = new W4._2_PWGenerator.PasswordGenerator();
+
+Console.WriteLine("Lütfen şifreniz için istediğiniz karakter sayısını giriniz.");
+
+var passwordLength = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Şifreniz sayıları içersin mi?");
+
+var includeNumbers = Console.ReadLine() == "E";
+
+Console.WriteLine("Şifreniz küçük karakterleri içersin mi?");
+
+var includeLowerCase = Console.ReadLine() == "E";
+
+Console.WriteLine("Şifreniz büyük karakterleri içersin mi?");
+
+var includeUpperCase = Console.ReadLine() == "E";
+
+Console.WriteLine("Şifreniz özel karakterleri içersin mi?");
+
+var includeSpecialChars = Console.ReadLine() == "E";
+
+var password = passwordGenerator.Generate(passwordLength, includeNumbers, includeLowerCase, includeUpperCase, includeSpecialChars);
+
+Console.WriteLine($"Şifreniz:{password}");
+
+Console.ReadLine();
