ba3670ea8c85b50a2b04c497db07436a7d0825f4
nkucukk <nkucuk097@gmail.com>
2023-10-19 15:12:30 +0300

    W6.1 RedoMusic Project

---
+++ W6.1-RedoMusic/Core/RedoMusic.Domain/Common/EntityBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReDoMusic.Domain.Common
+{
+    public class EntityBase<Tkey> :ICreatedOn,IDeletedOn,IModifiedOn
+    {
+
+        public Tkey Id { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? DeletedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+
+    }
+}
---
+++ W6.1-RedoMusic/Core/RedoMusic.Domain/Entities/Brand.cs
@@ -0,0 +1,20 @@
+﻿using ReDoMusic.Domain.Common; 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReDoMusic.Domain.Entities
+{
+    public class Brand : EntityBase<Guid>
+    {
+        public string Name { get; set; }
+
+        public string DisplayText { get; set; }
+
+        public string Address { get; set; }
+
+        //public DateTime EstablishDate { get; set; }
+    }
+}
