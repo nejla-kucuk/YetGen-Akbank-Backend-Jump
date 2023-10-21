4e20c8697c2893a63a531c2adfe54b7c6af178e1
nkucukk <nkucuk097@gmail.com>
2023-10-21 15:04:42 +0300

    changed

---
+++ W6.1-RedoMusicProject/Infrastructure/ReDoMusic.Persistance/Configurations.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReDoMusic.Persistance
+{
+    public static class Configurations
+    {
+
+        public static string GetString(string key)
+        {
+            ConfigurationManager configurationManager = new();
+
+            string path = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName}\\Infrastructure\\ReDoMusic.Persistance";
+
+            configurationManager.SetBasePath(path);
+
+            configurationManager.AddJsonFile("PrivateInformation.json");
+
+            return configurationManager.GetSection(key).Value;
+
+        }
+    }
+}
