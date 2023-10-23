27d9606e6ddf4240bda9e5d2a2b4288065ed6bd3
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-10-23 16:55:31 +0300

    [Featured]: Blazor

---
+++ W6.2-WebAPI/WebAPI.MVC/Controllers/CarsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using WebAPI.Domain.Entities;
+
+namespace WebAPI.MVC.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public CarsController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("Cars");
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+
+                var cars = await response.Content.ReadFromJsonAsync<List<Car>>();
+
+                return View(cars);
+            }
+            else if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+
+            }
+            else if (response.StatusCode == HttpStatusCode.BadRequest)
+            {
+
+            }
+
+            return NotFound();
+
+
+        }
+    }
+}
+
--- W6.2-WebAPI/WebAPI/Controllers/CarsController.cs
+++ W6.2-WebAPI/WebAPI/Controllers/CarsController.cs
@@ -8,12 +8,31 @@ namespace WebAPI.Controllers
     [ApiController]
     public class CarsController : ControllerBase
     {
+
         [HttpGet]
         public IActionResult GetAll() {
 
             var cars = CarsContext.LuxuryCars.ToList();
-        
+
             return Ok(cars);
+        }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetById(Guid id)
+        {
+            if(id == Guid.Empty)
+            {
+                return BadRequest("Error: id cannot be empty!");
+            }
+
+            var car = CarsContext.LuxuryCars.FirstOrDefault(x => x.Id == id);
+
+            if (car is null)
+            {
+                return NotFound("Error: The car requested with given Id was not found.");
+            }
+
+            return Ok(car);
         }
 
 
