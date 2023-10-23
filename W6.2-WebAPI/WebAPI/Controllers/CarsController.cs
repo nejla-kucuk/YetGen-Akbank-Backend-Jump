c683f43c0f371df9801d3759b17bf52492b61840
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-10-23 14:27:03 +0300

    [Feature]: WebAPI Domain + GetAll methods

---
+++ W6.2-WebAPI/WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Data;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll() {
+
+            var cars = CarsContext.LuxuryCars.ToList();
+        
+            return Ok(cars);
+        }
+
+
+    }
+}
