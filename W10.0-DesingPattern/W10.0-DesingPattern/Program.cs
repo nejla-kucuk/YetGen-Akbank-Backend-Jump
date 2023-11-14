7fe09aa2dc0878168076d3cc293f2c179578369d
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-11-14 19:36:52 +0300

    [Feat]: Desing Pattern

---
+++ W10.0-DesingPattern/W10.0-DesingPattern/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using W10._0_DesingPattern;
+
+// Uygulamada bir kez oluşturmak için kullanılır. Daha sonra yeniden çağırdığımızda eski instance gelir.
+
+Console.WriteLine("Singleton");
+
+ConfigurationService configurationService = new();
+
+var db = configurationService.GetValue("ConnectionStrings:ExampleDb");
+
+Console.WriteLine(db);
+
+
+
+
+Console.WriteLine("\n**************************************************\n");
+
+new MyClass();
+new MyClass();
+new MyClass();
+new MyClass();
+
+
+class MyClass
+{
+    // Sadece bir kez tetiklenir.
+    static MyClass()
+    {
+        Console.WriteLine("Static Constructor!");
+    }
+
+    public MyClass()
+    {
+        Console.WriteLine("Instance Created!");
+    }
+}
+
+
+
+
+
+
+
+
