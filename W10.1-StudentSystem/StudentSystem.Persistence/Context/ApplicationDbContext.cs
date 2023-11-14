1351acf0bb579209af1293e9faceca52e3dc41e0
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-11-14 13:44:33 +0300

    [Feat]: :droplet:  Persitence

---
+++ W10.1-StudentSystem/StudentSystem.Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using StudentSystem.Domain.Entites;
+
+namespace StudentSystem.Persistence.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
