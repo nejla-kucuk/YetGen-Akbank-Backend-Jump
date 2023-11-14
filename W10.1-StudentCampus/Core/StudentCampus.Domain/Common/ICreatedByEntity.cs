75ee27526c7e36540281e951996475e234b0cc46
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-11-14 22:28:27 +0300

    [Feat]: StudentCampus Domain

---
+++ W10.1-StudentCampus/Core/StudentCampus.Domain/Common/ICreatedByEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentCampus.Domain.Common
+{
+    public interface ICreatedByEntity
+    {
+        public string CreatedByUserId { get; set; }
+
+        public DateTimeOffset CreatedOn { get; set; }
+    }
+}
---
+++ W10.1-StudentCampus/W10.1-StudentCampus/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace W10._1_StudentCampus
+{
+    public class Class1
+    {
+
+    }
+}
