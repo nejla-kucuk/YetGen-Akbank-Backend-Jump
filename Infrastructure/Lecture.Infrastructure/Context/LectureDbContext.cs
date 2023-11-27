d8ef3a0878d2fe4fbcd9e7babedce866dfb78a5b
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-11-27 21:07:59 +0300

    [Feat]: Domain & Infrastructure & Presantation

---
+++ Infrastructure/Lecture.Infrastructure/Context/LectureDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Lecture.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture.Infrastructure.Context
+{
+    public class LectureDbContext:DbContext
+    {
+        public DbSet<Person> Person { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("LectureDB");
+        }
+    }
+}
---
+++ Presantation/Lecture.WebApi/Controllers/PersonsController.cs
@@ -0,0 +1,54 @@
+﻿using Lecture.Domain.Entities;
+using Lecture.Infrastructure.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lecture.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : Controller
+    {
+        private readonly LectureDbContext _context;
+
+        public PersonsController(LectureDbContext lectureDbContext)
+        {
+            _context = lectureDbContext;
+
+
+            List<Person> people = new List<Person>
+            {
+                new Person { Id = Guid.NewGuid(), FirstName = "John", LastName = "Doe" },
+                new Person { Id = Guid.NewGuid(), FirstName = "Jane", LastName = "Smith" },
+                new Person { Id = Guid.NewGuid(), FirstName = "Alice", LastName = "Johnson" },
+                new Person { Id = Guid.NewGuid(), FirstName = "Bob", LastName = "Brown" },
+                new Person { Id = Guid.NewGuid(), FirstName = "Charlie", LastName = "Davis" }
+            };
+            _context.Person.AddRange(people);
+            _context.SaveChanges();
+            Console.WriteLine(string.Join("\n", people.Select(x => x.Id).ToList()));
+        }
+
+
+        [HttpGet]
+        public string GetDefraudedPersonName(Guid personId)
+        {
+
+            var person = _context.Person.FirstOrDefault(x => x.Id == personId);
+
+            return person is null ? "Coulnd't Find!" : person.FirstName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+
+    
+}
+
+
+   
